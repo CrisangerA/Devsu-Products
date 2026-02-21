d34b347a4fa2218193aa7d66bbcbf3ef99a1b758
Alejandro Caicedo <caicedorivera1999@gmail.com>
2026-02-20 19:31:23 -0500

    feat(products): enhance product validation and form logic
    - Add length constraints for ID, name, and description fields
    - Ensure release date is current or future
    - Automatically set revision date to one year after release
    - Make revision date field non-editable
    - Add reset functionality to the form

--- src/modules/products/domain/product.scheme.ts
+++ src/modules/products/domain/product.scheme.ts
@@ -2,13 +2,24 @@ import z from 'zod';
 
 export const ProductSchema = z
   .object({
-    id: z.string().min(1, { message: 'ID es requerido' }),
-    name: z.string().min(1, { message: 'Nombre es requerido' }),
-    description: z.string().min(1, { message: 'Descripción es requerida' }),
+    id: z
+      .string()
+      .min(3, { message: 'ID debe tener mínimo 3 caracteres' })
+      .max(10, { message: 'ID debe tener máximo 10 caracteres' }),
+    name: z
+      .string()
+      .min(5, { message: 'Nombre debe tener mínimo 5 caracteres' })
+      .max(100, { message: 'Nombre debe tener máximo 20 caracteres' }),
+    description: z
+      .string()
+      .min(10, { message: 'Descripción debe tener mínimo 10 caracteres' })
+      .max(200, { message: 'Descripción debe tener máximo 200 caracteres' }),
     logo: z.string().min(1, { message: 'Logo es requerido' }),
     releaseDate: z
-      .date({ error: 'Fecha de liberación es requerida' })
-      .min(1, { message: 'Fecha de liberación es requerida' }),
+      .date({ message: 'Fecha de liberación es requerida' })
+      .min(new Date(), {
+        message: 'Fecha de liberación debe ser igual o mayor a la fecha actual',
+      }),
     revisionDate: z
       .date({ error: 'Fecha de revisión es requerida' })
       .min(1, { message: 'Fecha de revisión es requerida' }),
--- src/modules/products/ui/components/ProductsForm/ProductsForm.tsx
+++ src/modules/products/ui/components/ProductsForm/ProductsForm.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { useForm, useWatch } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 // Components
 import { Button } from '@components/core';
@@ -10,47 +10,64 @@ import {
   useProductMutationCreate,
   useProductMutationUpdate,
 } from '@modules/products/application/product.mutation';
-// Theme
-import { spacing } from '@theme/spacing';
-
+// Types
 import {
   ProductSchema,
   RegisterUserSchemaType,
 } from '@modules/products/domain/product.scheme';
-import { useNavigationProducts } from '@navigation/hooks/useNavigation';
+// Modules
 import { Product } from '@modules/products/domain/product.model';
+// Navigation
 import { ProductsRoutes } from '@navigation/config/routes';
+import { useNavigationProducts } from '@navigation/hooks/useNavigation';
+// Theme
+import { spacing } from '@theme/spacing';
 
 interface ProductsForm {
   product?: Product;
 }
 
 export default function ProductsForm({ product }: ProductsForm) {
+  // Hooks
   const { goBack, popTo } = useNavigationProducts();
+  const { control, handleSubmit, setError, reset, setValue } =
+    useForm<RegisterUserSchemaType>({
+      resolver: zodResolver(ProductSchema),
+      defaultValues: {
+        id: product?.id || '',
+        name: product?.name || '',
+        description: product?.description || '',
+        logo: product?.logo || '',
+        releaseDate: product?.releaseDate
+          ? new Date(product?.releaseDate || '')
+          : undefined,
+        revisionDate: product?.revisionDate
+          ? new Date(product?.revisionDate || '')
+          : undefined,
+      },
+    });
 
-  const { control, handleSubmit, setError } = useForm<RegisterUserSchemaType>({
-    resolver: zodResolver(ProductSchema),
-    defaultValues: {
-      id: product?.id || '',
-      name: product?.name || '',
-      description: product?.description || '',
-      logo: product?.logo || '',
-      releaseDate: product?.releaseDate
-        ? new Date(product?.releaseDate || '')
-        : undefined,
-      revisionDate: product?.revisionDate
-        ? new Date(product?.revisionDate || '')
-        : undefined,
-    },
-  });
-
+  // Mutations
   const { mutate: createProduct, isPending: isCreating } =
     useProductMutationCreate();
   const { mutate: updateProduct, isPending: isUpdating } =
     useProductMutationUpdate();
 
-  const isLoading = isCreating || isUpdating;
+  // Watch releaseDate and set revisionDate to one year later
+  const releaseDate = useWatch({
+    control,
+    name: 'releaseDate',
+  });
 
+  useEffect(() => {
+    if (releaseDate) {
+      const revisionDate = releaseDate;
+      revisionDate.setFullYear(releaseDate.getFullYear() + 1);
+      setValue('revisionDate', revisionDate);
+    }
+  }, [releaseDate, setValue]);
+
+  // Events
   function onSubmit(form: RegisterUserSchemaType) {
     if (product?.id) {
       updateProduct(
@@ -108,6 +125,9 @@ export default function ProductsForm({ product }: ProductsForm) {
     }
   }
 
+  // Constants
+  const isLoading = isCreating || isUpdating;
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -146,6 +166,7 @@ export default function ProductsForm({ product }: ProductsForm) {
         name="revisionDate"
         placeholder="Nombre"
         label="Fecha Revisión"
+        editable={false}
       />
 
       <View style={styles.actions}>
@@ -158,7 +179,12 @@ export default function ProductsForm({ product }: ProductsForm) {
           disabled={isLoading}
         />
 
-        <Button title="Reiniciar" variant="secondary" disabled={isLoading} />
+        <Button
+          title="Reiniciar"
+          variant="secondary"
+          disabled={isLoading}
+          onPress={() => reset()}
+        />
       </View>
     </View>
   );
